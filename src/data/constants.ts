4a1ebe6080f3c95b94c9f09ef34fdf622e327115
Thea Mushambadze <thea.mushambadze@gmail.com>
2023-04-06 19:18:36 +0400

    update

---
+++ src/data/constants.ts
@@ -0,0 +1,18 @@
+export const MENU_LINKS = [
+	{
+		title: "Home",
+		path: "/",
+	},
+	{
+		title: "Blog",
+		path: "/posts",
+	},
+];
+
+
+export const SOCIAL_LINKS = {
+	github: "https://github.com/highflyer910",
+	twitter: "https://twitter.com/highflyer910",
+	discord: "https://discordapp.com/users/283253836234096642",
+	email: "mushambadzeteona@gmail.com",
+};
